8b309293636fab6978cb8b5ff7e5ddea29d02e56
Ravindu007 <ravindu0504@gmail.com>
2023-12-02 20:42:27 +0530

    add components & routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component:BookListComponent
+  },
+  {
+    path:"add-book",
+    component: AddBookComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { AddBookComponent } from './components/add-book/add-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BookListComponent,
+    AddBookComponent
   ],
   imports: [
     BrowserModule,
